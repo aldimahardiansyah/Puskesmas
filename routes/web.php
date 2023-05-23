3f9471db8a99ba00107098f382ae6645ef2ddd67
aldimahardiansyah <aldimdiansyah@gmail.com>
2023-05-23 16:11:23 +0700

    praktikum pertemuan 10

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PasienController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route untuk menampilkan dashboard admin
+Route::get('/', [DashboardController::class, 'index']);
 
 // Route untuk menampilkan daftar pasien
 Route::get('/pasien', [PasienController::class, 'index']);
