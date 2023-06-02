1e8253011ec5583d62342362379e9f3011adccd1
aldimahardiansyah <aldimdiansyah@gmail.com>
2023-06-02 13:20:30 +0700

    praktikum pertemuan 11

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -7,9 +7,10 @@ use Illuminate\Http\Request;
 
 class PasienController extends Controller
 {
+    // menampilkan semua data pasien
     public function index()
     {
-        $pasiens = Pasien::getAll();
+        $pasiens = Pasien::all();
         return view('admin.pasien.index', [
             'pasiens' => $pasiens
         ]);
@@ -22,6 +23,15 @@ class PasienController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
+        // Simpan data hasil form ke tabel pasiens
+        Pasien::create([
+            'nama' => $request->nama,
+            'jk' => $request->jk,
+            'tgl_lahir' => $request->tgl_lahir,
+            'alamat' => $request->alamat,
+            'telp' => $request->telp,
+        ]);
+
+        return redirect('/pasien');
     }
 }
--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -9,30 +9,9 @@ class Pasien extends Model
 {
     use HasFactory;
 
-    public static function getAll()
-    {
-        return [
-            [
-                "nama" => "Andi",
-                "jk" => 'l',
-                "tgl_lahir" => "12/03/2002",
-                "alamat" => "Bogor",
-                "telp" => "08123456789"
-            ],
-            [
-                "nama" => "Sifa",
-                "jk" => 'p',
-                "tgl_lahir" => "02/03/2003",
-                "alamat" => "Depok",
-                "telp" => "08123456789"
-            ],
-            [
-                "nama" => "Latif",
-                "jk" => 'l',
-                "tgl_lahir" => "10/06/2002",
-                "alamat" => "Jakarta",
-                "telp" => "08123456789"
-            ],
-        ];
-    }
+    // menghubungkan model ke table pasiens
+    protected $table = 'pasiens';
+
+    // mendeklarasikan kolom yang boleh diisi
+    protected $fillable = ['nama', 'jk', 'alamat', 'tgl_lahir', 'telp'];
 }
