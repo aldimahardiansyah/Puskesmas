7b4759e1599d918f07dd7fb19a03ed599861a382
aldimahardiansyah <aldimdiansyah@gmail.com>
2023-06-09 16:06:56 +0700

    praktukm 12

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -24,6 +24,15 @@ class PasienController extends Controller
     // method untuk menyimpan data ke database
     public function store(Request $request)
     {
+        // validasi data
+        $request->validate([
+            'nama' => 'required|min:3',
+            'jk' => 'required',
+            'tgl_lahir' => 'required|date',
+            'alamat' => 'required',
+            'telp' => 'required|numeric',
+        ]);
+
         Pasien::create([
             'nama' => $request->nama,
             'jk' => $request->jk,
@@ -33,5 +42,53 @@ class PasienController extends Controller
         ]);
 
         return redirect('/pasien');
+    }
+
+    public function edit($id)
+    {
+        // cari pasien berdasarkan id
+        $pasien = Pasien::find($id);
+
+        return view('pasien.edit', [
+            'pasien' => $pasien,
+        ]);
+    }
+
+    public function update($id, Request $request)
+    {
+        // validasi data
+        $request->validate([
+            'nama' => 'required|min:3',
+            'jk' => 'required',
+            'tgl_lahir' => 'required|date',
+            'alamat' => 'required',
+            'telp' => 'required|numeric',
+        ]);
+
+        // cari pasien berdasarkan id
+        $pasien = Pasien::find($id);
+
+        // simpan perubahan
+        $pasien->update([
+            'nama' => $request->nama,
+            'jk' => $request->jk,
+            'tgl_lahir' => $request->tgl_lahir,
+            'alamat' => $request->alamat,
+            'telp' => $request->telp,
+        ]);
+
+        return redirect('/pasien')->with('success', 'Data pasien berhasil diubah.');
+    }
+
+    // method untuk menghapus
+    public function destroy(Request $request)
+    {
+        $deleted = Pasien::destroy($request->id);
+
+        if ($deleted) {
+            return redirect('/pasien')->with('success', 'Data pasien berhasil dihapus.');
+        }
+
+        return redirect('/pasien')->with('success', 'Data pasien gagal dihapus.');
     }
 }
