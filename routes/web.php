1a2fcca6a822e444c36d9e80d1dba89c42e8079f
aldimahardiansyah <aldimdiansyah@gmail.com>
2023-06-13 15:27:31 +0700

    praktikum 13

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PasienController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,22 +17,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route untuk menampilkan dashboard admin
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->middleware('auth');
 
 // Route untuk menampilkan daftar pasien
-Route::get('/pasien', [PasienController::class, 'index']);
+Route::get('/pasien', [PasienController::class, 'index'])->middleware('auth');
 
 // Route untuk menampilkan form tambah pasien
-Route::get('/pasien/create', [PasienController::class, 'create']);
+Route::get('/pasien/create', [PasienController::class, 'create'])->middleware('auth');
 
 // Route untuk memproses form tambah pasien
-Route::post('/pasien', [PasienController::class, 'store']);
+Route::post('/pasien', [PasienController::class, 'store'])->middleware('auth');
 
 // Route untuk menampilkan form edit pasien
-Route::get('/pasien/edit/{id}', [PasienController::class, 'edit']);
+Route::get('/pasien/edit/{id}', [PasienController::class, 'edit'])->middleware('auth');
 
 // Route untuk memproses form edit pasien
-Route::put('/pasien/{id}', [PasienController::class, 'update']);
+Route::put('/pasien/{id}', [PasienController::class, 'update'])->middleware('auth');
 
 // Route untuk hapus pasien
-Route::delete('/pasien', [PasienController::class, 'destroy']);
+Route::delete('/pasien', [PasienController::class, 'destroy'])->middleware('auth');
+
+Auth::routes();
