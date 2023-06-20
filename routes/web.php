b450514a71817170792aa9337fb447e7943ac918
aldimahardiansyah <aldimdiansyah@gmail.com>
2023-06-20 15:35:57 +0700

    praktikum 14

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route untuk menampilkan dashboard admin
-Route::get('/', [DashboardController::class, 'index'])->middleware('auth');
+// route untuk role admin
+Route::group(['middleware' => ['auth', 'role:admin']], function () {
+    // Route untuk menampilkan form tambah pasien
+    Route::get('/pasien/create', [PasienController::class, 'create']);
 
-// Route untuk menampilkan daftar pasien
-Route::get('/pasien', [PasienController::class, 'index'])->middleware('auth');
+    // Route untuk memproses form tambah pasien
+    Route::post('/pasien', [PasienController::class, 'store']);
 
-// Route untuk menampilkan form tambah pasien
-Route::get('/pasien/create', [PasienController::class, 'create'])->middleware('auth');
+    // Route untuk menampilkan form edit pasien
+    Route::get('/pasien/edit/{id}', [PasienController::class, 'edit']);
 
-// Route untuk memproses form tambah pasien
-Route::post('/pasien', [PasienController::class, 'store'])->middleware('auth');
+    // Route untuk memproses form edit pasien
+    Route::put('/pasien/{id}', [PasienController::class, 'update']);
 
-// Route untuk menampilkan form edit pasien
-Route::get('/pasien/edit/{id}', [PasienController::class, 'edit'])->middleware('auth');
+    // Route untuk hapus pasien
+    Route::delete('/pasien', [PasienController::class, 'destroy']);
+});
 
-// Route untuk memproses form edit pasien
-Route::put('/pasien/{id}', [PasienController::class, 'update'])->middleware('auth');
+// Route untuk user terautentikasi
+Route::group(['middleware' => ['auth']], function () {
+    // Route untuk menampilkan dashboard admin
+    Route::get('/', [DashboardController::class, 'index']);
 
-// Route untuk hapus pasien
-Route::delete('/pasien', [PasienController::class, 'destroy'])->middleware('auth');
+    // Route untuk menampilkan daftar pasien
+    Route::get('/pasien', [PasienController::class, 'index']);
+});
 
 Auth::routes();
